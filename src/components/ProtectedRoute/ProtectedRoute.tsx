f9012c5e9cc507aa85650e00feaafc2272235892
teradaian <teradaian@gmail.com>
2023-02-08 11:39:02 -0800

    convert protectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,6 +1,13 @@
+import { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
+import { User } from '../../interfaces/user.model'
 
-const ProtectedRoute = ({ user, children }) => {
+type ProtectedRouteProps = {
+  user: User,
+  children?: ReactNode
+}
+
+const ProtectedRoute = ({ user, children }: ProtectedRouteProps) => {
   if (!user) { return <Navigate to="/login" /> }
   return children
 }
