42466f403e73ccd507704aaa1e87ac02f1fc7465
teradaian <teradaian@gmail.com>
2023-02-08 10:48:32 -0800

    add pages and components

---
+++ src/pages/Profiles/Profiles.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import * as profileService from '../../services/profileService'
+
+const Profiles = () => {
+  const [profiles, setProfiles] = useState([])
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const profileData = await profileService.getAllProfiles()
+      setProfiles(profileData)
+    }
+    fetchProfiles()
+  }, [])
+
+  return (
+    <>
+      <h1>Hello. This is a list of all the profiles.</h1>
+      {profiles.length ? 
+        <>
+          {profiles.map(profile =>
+            <p key={profile._id}>{profile.name}</p>
+          )}
+        </>
+      :
+        <p>No profiles yet</p>
+      }
+    </>
+  )
+}
+ 
+export default Profiles
