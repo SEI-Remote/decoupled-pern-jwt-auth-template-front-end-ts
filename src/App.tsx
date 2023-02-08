d6426f79b55eafa595bf1a1bc850b46b20efb35a
teradaian <teradaian@gmail.com>
2023-02-08 10:46:44 -0800

    structure app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// npm modules 
+import { useState } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+
+// page components
+
+// components
+
+// services
+
+// styles
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+    
+    </div>
+  )
+}
+
+export default App
